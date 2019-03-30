64c0d3e3f7c8fb00261b33e414ad3fc693120d79
unknown <magurawork@gmail.com>
2019-03-30 14:06:55 +0300

    add changes - block job

--- project/js/common.js
+++ project/js/common.js
@@ -1,34 +1,49 @@
 $(document).ready(function() {
 
-    $('.language-item a').on('click', function(){
-      $('.language-item a').removeClass('current');
-      $(this).addClass('current');
-    });
+  // Scroll to block
+  $(".navigation-item").on("click", "a", function(event) {
+    event.preventDefault();
+    var id = $(this).attr('href'),
+      top = $(id).offset().top;
+    $('body,html').animate({
+      scrollTop: top - 15
+    }, 1000);
+  });
 
-    $('.navigation-item a').on('click', function () {
-      $('.navigation-item a').removeClass('current');
-      $(this).addClass('current');
-    });
+  $('.language-item a').on('click', function(){
+    $('.language-item a').removeClass('current');
+    $(this).addClass('current');
+  });
 
-    $('.navigation-btn').on('click', function(){
-      $(this).toggleClass('active');
-      $('.navigation-list').toggleClass('show');
-    });
+  $('.navigation-item a').on('click', function () {
+    $('.navigation-item a').removeClass('current');
+    $(this).addClass('current');
+  });
 
-    if ($(window).width() <= 768) {
-      $('.navigation-list').removeClass('show');
-    }
+  $('.navigation-btn').on('click', function(){
+    $(this).toggleClass('active');
+    $('.navigation-list').toggleClass('show');
+  });
+
+  if ($(window).width() <= 768) {
+    $('.navigation-list').removeClass('show');
+  }
 
   $('.work-link').on('click', function(e) {
     e.preventDefault();
     var k = $(this).data('target');
     $('#' + k + '').addClass('opened');
-
-    });
-
-
+  });
   
+  $('.close-btn').on('click', function(e){
+    e.preventDefault();    
+    $('.work-full').removeClass('opened');
+  });
+  $('.close-btn').on('hover', function(e){
+    e.preventDefault();    
+    $(this).addClass('clicked');
+  });
+
 
 });
-
 
